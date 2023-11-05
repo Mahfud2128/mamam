c21d14e90d9543b082c6a148c56773a1347afde7
Mahfud2128 <87294788+Mahfud2128@users.noreply.github.com>
2023-11-05 19:04:05 +0700

    Update add-trgrpc.sh

--- add-trgrpc.sh
+++ add-trgrpc.sh
@@ -23,7 +23,9 @@ echo -e "${BB}——————————————————————
 echo -e "${YB}A client with the specified name was already created, please choose another name.${NC}"
 echo -e "${BB}————————————————————————————————————————————————————${NC}"
 read -n 1 -s -r -p "Press any key to back on menu"
-addtrgrpc
+add-trgrpc
+fi
+done
 read -p "Expired (days): " masaaktif
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
 sed -i '/#xtrgrpc$/a\#& '"$user $exp"'\
@@ -50,3 +52,42 @@ Host: $domain
 Link TLS  : trojan://$uuid@$domain:443?path=/trojan&security=tls&host=$domain&type=ws&sni=$domain#$user
 ==========================
 END
+ISP=$(cat /usr/local/etc/xray/org)
+CITY=$(cat /usr/local/etc/xray/city)
+systemctl restart xray
+clear
+echo -e "————————————————————————————————————————————————————" | tee -a /user/log-trojan-$user.txt
+echo -e "                   Trojan Account                  " | tee -a /user/log-trojan-$user.txt
+echo -e "————————————————————————————————————————————————————" | tee -a /user/log-trojan-$user.txt
+echo -e "Remarks       : ${user}" | tee -a /user/log-trojan-$user.txt
+echo -e "ISP           : $ISP" | tee -a /user/log-trojan-$user.txt
+echo -e "City          : $CITY" | tee -a /user/log-trojan-$user.txt
+echo -e "Host/IP       : ${domain}" | tee -a /user/log-trojan-$user.txt
+echo -e "Wildcard      : (bug.com).${domain}" | tee -a /user/log-trojan-$user.txt
+echo -e "Port TLS      : 443" | tee -a /user/log-trojan-$user.txt
+echo -e "Port NTLS     : 80" | tee -a /user/log-trojan-$user.txt
+echo -e "Port gRPC     : 443" | tee -a /user/log-trojan-$user.txt
+echo -e "Alt Port TLS  : 2053, 2083, 2087, 2096, 8443" | tee -a /user/log-trojan-$user.txt
+echo -e "Alt Port NTLS : 8080, 8880, 2052, 2082, 2086, 2095" | tee -a /user/log-trojan-$user.txt
+echo -e "Password      : ${uuid}" | tee -a /user/log-trojan-$user.txt
+echo -e "Network       : Websocket, gRPC" | tee -a /user/log-trojan-$user.txt
+echo -e "Path          : /trojan" | tee -a /user/log-trojan-$user.txt
+echo -e "ServiceName   : trojan-grpc" | tee -a /user/log-trojan-$user.txt
+echo -e "Alpn          : h2, http/1.1" | tee -a /user/log-trojan-$user.txt
+echo -e "————————————————————————————————————————————————————" | tee -a /user/log-trojan-$user.txt
+echo -e "Link TLS      : ${trojanlink1}" | tee -a /user/log-trojan-$user.txt
+echo -e "————————————————————————————————————————————————————" | tee -a /user/log-trojan-$user.txt
+echo -e "Link NTLS     : ${trojanlink2}" | tee -a /user/log-trojan-$user.txt
+echo -e "————————————————————————————————————————————————————" | tee -a /user/log-trojan-$user.txt
+echo -e "Link gRPC     : ${trojanlink3}" | tee -a /user/log-trojan-$user.txt
+echo -e "————————————————————————————————————————————————————" | tee -a /user/log-trojan-$user.txt
+echo -e "Format Clash  : http://$domain:8000/trojan/trojan-$user.txt" | tee -a /user/log-trojan-$user.txt
+echo -e "————————————————————————————————————————————————————" | tee -a /user/log-trojan-$user.txt
+echo -e "Expired On    : $exp" | tee -a /user/log-trojan-$user.txt
+echo -e "————————————————————————————————————————————————————" | tee -a /user/log-trojan-$user.txt
+echo " " | tee -a /user/log-trojan-$user.txt
+echo " " | tee -a /user/log-trojan-$user.txt
+echo " " | tee -a /user/log-trojan-$user.txt
+read -n 1 -s -r -p "Press any key to back on menu"
+clear
+trojan
